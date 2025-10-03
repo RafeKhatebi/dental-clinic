21fc74d5104273c8b911b3f8dfad61f6a3869d39
Rafe Khatebi <rkhatibi2003@gmail.com>
2025-10-03 08:08:28 -0700

    deleted the unsued and redunadancey data

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?php
-require_once 'config/config_optimized.php';
+require_once 'config/config.php';
 
 if (isLoggedIn()) {
     logActivity('logout', 'users', $_SESSION['user_id'], 'User logged out');
     session_destroy();
 }
 
-redirect('/index_optimized.php');
+redirect('/index.php');
 ?>
